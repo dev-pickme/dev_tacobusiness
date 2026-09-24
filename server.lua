1b1c4c3ebce9f71f9b3f6082b7c0e45b9c4ab8fa
dev-pickme <ghffrnyrms@gmail.com>
2026-09-24 10:31:56 +0700

    Fix storage stash comments and adjust item counts for Taco Stash functionality

--- server.lua
+++ server.lua
@@ -16,7 +16,7 @@ lib.callback.register('tacobusiness:server:openTacoTray', function(source)
 
 end)
 
--- Storage Stash
+-- Storage Stash 2
 lib.callback.register('tacobusiness:server:openTacoStash', function(source)
 	local Player = exports['qbx_core']:GetPlayer(source)
 	if not(Player and Player.PlayerData.job.name == 'tacobusiness') then
@@ -27,8 +27,27 @@ lib.callback.register('tacobusiness:server:openTacoStash', function(source)
 	exports.ox_inventory:RegisterStash(
 		stashId,
 		'Storage 1',
-		50,
-		10000,
+		20,
+		20000,
+		true,
+		'tacobusiness'
+	)
+	return stashId
+end)
+
+-- Storage Stash 2
+lib.callback.register('tacobusiness:server:openTacoStash', function(source)
+	local Player = exports['qbx_core']:GetPlayer(source)
+	if not(Player and Player.PlayerData.job.name == 'tacobusiness') then
+		return nil
+	end
+	
+	local stashId = 'taco_storage_1'
+	exports.ox_inventory:RegisterStash(
+		stashId,
+		'Storage 2',
+		20,
+		20000,
 		true,
 		'tacobusiness'
 	)
